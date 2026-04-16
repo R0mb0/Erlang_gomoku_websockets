d75f570fd5b41633d3d66a7883537add463b94cf
Francesco Rombaldoni <f.rombaldoni@campus.uniurb.it>
2026-04-16 21:42:46 +0200

    Added new parts of code :)

--- Gomoku/gomoku_backend/src/gomoku_backend_app.erl
+++ Gomoku/gomoku_backend/src/gomoku_backend_app.erl
@@ -1,13 +1,23 @@
 -module(gomoku_backend_app).
 -behaviour(application).
 
-%% --- Application Callbacks ---
 -export([start/2, stop/1]).
 
-%% This is the absolute starting point of our application.
 start(_StartType, _StartArgs) ->
-    %% For now, we just tell our main Supervisor to wake up.
-    %% In the next step, we will inject the Cowboy WebSocket routing here!
+    %% 1. Define Cowboy Routes. Traffic to ws://localhost:8080/ws goes to gomoku_ws_handler
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/ws", gomoku_ws_handler, []}
+        ]}
+    ]),
+
+    %% 2. Start the Cowboy HTTP/WebSocket server on port 8080
+    {ok, _} = cowboy:start_clear(http_listener,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+
+    %% 3. Start our Supervisor (which starts Tuple Space and Gomoku Server)
     gomoku_backend_sup:start_link().
 
 stop(_State) ->
